b87706351913f8c467fb4db11ad512099fc8227c
Mahder Tesfaye Abebe <mahdertesfaye11@gmail.com>
2025-03-31 13:09:16 +0300

    changed button color

--- Frontend_Staff/src/pages/Staff/Staff.jsx
+++ Frontend_Staff/src/pages/Staff/Staff.jsx
@@ -298,7 +298,7 @@ const AddStaffForm = ({ onSave, onCancel }) => {
           </button>
           <button
             type="submit"
-            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           >
             Save
           </button>
