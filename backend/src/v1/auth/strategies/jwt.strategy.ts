5a012bd96b87db1feabd251ea24c4cf65d08e9f9
Ashenafi Tsegaye <ashenafitsegish@gmail.com>
2025-04-18 09:35:00 +0300

    final

--- backend/src/v1/auth/strategies/jwt.strategy.ts
+++ backend/src/v1/auth/strategies/jwt.strategy.ts
@@ -4,38 +4,10 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { Request } from 'express';
 
-<<<<<<< HEAD
-//JwtStrategy is used for validating JWT tokens for regular users.
-@Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor() {
-    super({
-      // Extract JWT token from the Authorization header ("Bearer <token>")
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      // Secret key for verifying the token; Non-null assertion used because it's assumed to be defined.
-      secretOrKey: process.env.JWT_SECRET as string,
-    });
-  }
-
-  // This method is automatically called after successful token verification.
-  validate(payload: any) {
-    return { userId: payload.sub, email: payload.email };
-  }
-}
-
-// StaffJwtStrategy is used for validating JWT tokens for staff members.
-=======
->>>>>>> 7fabc87481018b0af37b37982d03386f7177f4d2
 @Injectable()
 export class StaffJwtStrategy extends PassportStrategy(Strategy, 'staff-jwt') {
   constructor() {
     super({
-<<<<<<< HEAD
-      // Extract JWT token from the Authorization header ("Bearer <token>")
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      // Secret key for verifying staff tokens; Non-null assertion used because it's assumed to be defined.
-      secretOrKey: process.env.STAFF_JWT_SECRET as string
-=======
       jwtFromRequest: ExtractJwt.fromExtractors([
         (request: Request) => {
           // Check for token in cookies first
@@ -48,7 +20,6 @@ export class StaffJwtStrategy extends PassportStrategy(Strategy, 'staff-jwt') {
       ]),
       ignoreExpiration: false,
       secretOrKey: process.env.STAFF_JWT_SECRET,
->>>>>>> 7fabc87481018b0af37b37982d03386f7177f4d2
     });
   }
 
